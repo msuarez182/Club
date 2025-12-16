51a73fff2f4f086eaa8f30f8ed022e341a85c7d3
Miguel Suarez <msuarez@conectimed.com>
2025-12-16 10:20:04 -0600

    Se agrega DotEnv para optimizar el uso de credenciales del club

--- includes/database/dbClub.php
+++ includes/database/dbClub.php
@@ -1,13 +1,18 @@
 <?php
 
-use App\ActiveRecord;
 require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../funciones.php';
 
 
-function dbClub(){
-    $dbClub= new Mysqli('localhost','root','maiki','club');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
+$dotenv->safeLoad();
+
+
+function dbClub()
+{
+    $dbClub = new Mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
     $dbClub->set_charset("utf8");
     return $dbClub;
 }
 
-$dbClub=dbClub();
+$dbClub = dbClub();
--- includes/database/dbMedicable.php
+++ includes/database/dbMedicable.php
@@ -7,13 +7,16 @@ use App\ActiveRecord;
 
 function dbMedicable()
 {
-$servidor = "localhost";
-$usuario = "root";
-$clave = "maiki";
-$baseDeDatos = "medicable_nversion";
+
+    
+
+// $servidor = "localhost";
+// $usuario = "root";
+// $clave = "maiki";
+// $baseDeDatos = "medicable_nversion";
 
 
-    $dbMedicable = new Mysqli($servidor, $usuario, $clave, $baseDeDatos);
+    $dbMedicable = new Mysqli($_ENV['DBM_HOST'],$_ENV['DBM_USER'],$_ENV['DBM_PASS'],$_ENV['DBM_NAME']);
     $dbMedicable->set_charset("utf8");
     if (!$dbMedicable) {
         return die("Conexi��n fallida: " . mysqli_connect_error());
