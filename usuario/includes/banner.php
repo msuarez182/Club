560a2cb17b67781badddcc151dc9e1df43fa9d30
Miguel Suarez <msuarez@conectimed.com>
2026-01-05 10:21:20 -0600

    se quito el banner de ejemplo por un random

--- usuario/includes/banner.php
+++ usuario/includes/banner.php
@@ -7,9 +7,9 @@
       <!-- Banner 1 -->
       <div class="carousel-item active h-100">
         <a href="https://ejemplo.com/promocion1" target="_blank">
-          <img src="../usuario/assets/img/prueba1.png" 
+          <img src="https://picsum.photos/1400/300.webp?random=1" 
                class="d-block w-100 h-100 object-fit-cover" 
-               alt="Promoci�n exclusiva"
+               alt="Promoción exclusiva"
                loading="lazy"
                width="1400"
                height="300">
